387749c1c71b082e5e7e924330b65158673aaae6
Oguzhan Uzman <ouzmann@gmail.com>
2020-12-02 01:47:04 +0300

    terraform example added

---
+++ example.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0ce1e3f77cd41957e"
+  instance_type = "t2.micro"
+}
