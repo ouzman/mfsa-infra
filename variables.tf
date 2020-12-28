bdbda6f0ccc51e721169616eac2a4faf0f6010ed
Oguzhan Uzman <oguzhan.uzman@microfocus.com>
2020-12-28 23:04:22 +0300

    try to fix variable syntax

--- variables.tf
+++ variables.tf
@@ -6,9 +6,10 @@ variable "lambda_bucket" {
     type = string
 }
 
-variable "facebook" {
-  type = object({
-    client_id = string
-    client_secret = string
-  })
+variable "facebook_client_id" {
+  type = string
+}
+
+variable "facebook_client_secret" {
+  type = string
 }
