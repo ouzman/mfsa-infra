fc93187f11c46b2e51a80abc805bdc3fc9dd793d
Oguzhan Uzman <oguzhan.uzman@microfocus.com>
2020-12-29 00:47:26 +0300

    test outputs

--- cognito-user-pool/outputs.tf
+++ cognito-user-pool/outputs.tf
@@ -1,4 +1,3 @@
 output "ios_client_secret" {
-    value       = aws_cognito_user_pool_client.ios_client.client_secret
-    sensitive   = true
+    value = aws_cognito_user_pool_client.ios_client.client_secret
 }
