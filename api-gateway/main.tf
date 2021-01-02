34bb554e6b5d7cbc5d0cbe8b15bff6323fc1ba88
Oguzhan Uzman <oguzhan.uzman@microfocus.com>
2021-01-02 05:06:43 +0300

    api path fix

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -40,7 +40,7 @@ resource "aws_api_gateway_resource" "file_resource" {
 
 resource "aws_api_gateway_method" "file_resource_method" {
   rest_api_id = aws_api_gateway_rest_api.share_api.id
-  resource_id = aws_api_gateway_resource.specific_file_resource.id
+  resource_id = aws_api_gateway_resource.file_resource.id
   http_method = "GET"
 
   authorization = "COGNITO_USER_POOLS"
